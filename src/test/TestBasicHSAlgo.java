97d0a2f5209c6200f34a14786953d67fca11c7f9
VARADHAN <varadhan1995nitt@gmail.com>
2020-02-10 19:22:45 -0600

    small change in test case

--- src/test/TestBasicHSAlgo.java
+++ src/test/TestBasicHSAlgo.java
@@ -3,24 +3,23 @@ package test;
 import org.testng.annotations.Test;
 import impl.HSAlgoImpl;
 import interfaces.HSAlgo;
-import junit.framework.Assert;
 import util.ProcessUID;
+import org.junit.Assert;
 
 public class TestBasicHSAlgo {
-  @SuppressWarnings("deprecation")
   @Test
-  public void f() {
-    int [] UID = new int [] {10,20,30,40,50,60,70,80,90,100,120};
+  public void simpleHSAlgoTest() {
+    int[] UID = new int[] {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120};
     int num_of_processes = UID.length;
-    ProcessUID [] processUIDObject = new ProcessUID[num_of_processes];
-    for(int i=0;i<num_of_processes;i++) {
+    ProcessUID[] processUIDObject = new ProcessUID[num_of_processes];
+    for (int i = 0; i < num_of_processes; i++) {
       processUIDObject[i] = new ProcessUID(i, UID[i]);
     }
-    
+
     HSAlgo hsAlgo = new HSAlgoImpl(num_of_processes, processUIDObject);
     ProcessUID leader = hsAlgo.execute();
-    
-    Assert.assertEquals(leader.getProcessUID(), 120);
-    
+
+    Assert.assertEquals(120, leader.getProcessUID());
+
   }
 }
